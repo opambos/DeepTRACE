d3bcfc935a0bd837ece4643e3a7b8327234e2f00
opambos <149190022+opambos@users.noreply.github.com>
2024-02-29 19:11:49 +0000

    Overhaul fluorescence video file handling to enable TIF/TIFF, and dynamically capture any missing metadata with check and prompt to user prior to data prepartion

--- extractVideo.m
+++ extractVideo.m
@@ -1,5 +1,5 @@
 function [video] = extractVideo(ffpath, ffname, frame_lo, frame_hi, x_lo, x_hi, y_lo, y_hi)
-%Extracts a section (in x, y, t) of a FITS image sequence, Oliver Pambos,
+%Extracts a section (in x, y, t) of a video image sequence, Oliver Pambos,
 %10/11/2020.
 %oliver.pambos@physics.ox.ac.uk
 %
@@ -29,10 +29,28 @@ function [video] = extractVideo(ffpath, ffname, frame_lo, frame_hi, x_lo, x_hi, 
 %WITH POTENTIALLY DIFFERENT USAGE CONDITIONS.
 %
 %
+%This function handles cropping of video stacks in both FITS and TIF/TIFF
+%formats. For FITS files, the performance is optimised for FITS fils as the
+%function only extracts the specified ROI, and performs extraction across
+%the entire stack with a single call. This approach improves processing
+%speed and minimizes memory usage.
+%
+%However, for TIF/TIFF files, it accesses only the relevant frames but
+%requires the entire frame to be read into memory before ROI extraction due
+%to limitations in TIF format handling. This leads to potentially slower
+%performance and more memory usage, particularly when using the image size
+%is much larger than the ROI.
+%
+%Note also that it was necessary to suppress the warning
+%'imageio:tiffutils:libtiffWarning'. As it is unclear how this impacts
+%performance, if this function is called repetitively it would be best to
+%move the warning suppression code to encapsulate the repetitive external
+%call.
+%
 %Inputs
 %------
-%ffpath     (str)   FITS file path
-%ffname     (str)   FITS file name
+%ffpath     (str)   video file path
+%ffname     (str)   video file name
 %frame_lo   (int)   first frame of subset of images to extract
 %frame_hi   (int)   final frame of subset of images to extract
 %x_lo       (vec)   pixel position of left edge of the ROI to extract
@@ -48,10 +66,56 @@ function [video] = extractVideo(ffpath, ffname, frame_lo, frame_hi, x_lo, x_hi, 
 %-----------------------------------------
 %matlab.io.fits package reqiured for handling FITS files
 
-    import matlab.io.*
-    fptr = fits.openFile(char(fullfile(ffpath,ffname)));    %17/06/2022: added char() here to eliminate a bug
-    video = fits.readImg(fptr, [y_lo x_lo frame_lo], [y_hi x_hi frame_hi]);
-    fits.closeFile(fptr);
+    
+    %extract file extension
+    [~, ~, ext] = fileparts(ffname);
+    ext = lower(ext);
+    if strcmp(ext, '.tif') || strcmp(ext, '.tiff')
+        file_type = '.tif';
+    else
+        file_type = ext;
+    end
+    
+    video = [];
+    
+    %extract cropped region of video
+    switch file_type
+        case '.fits'
+            import matlab.io.*
+            fptr = fits.openFile(char(fullfile(ffpath,ffname)));    %17/06/2022: char() as this can be passed as a cell array
+            video = fits.readImg(fptr, [y_lo x_lo frame_lo], [y_hi x_hi frame_hi]);
+            fits.closeFile(fptr);
+            
+        case '.tif'
+            tif_meta = imfinfo(char(fullfile(ffpath, ffname)));
+            N_frames = numel(tif_meta);
+            
+            %pre-allocate
+            video = zeros(y_hi-y_lo+1, x_hi-x_lo+1, min(frame_hi, N_frames)-frame_lo+1, 'uint16');
+            
+            %suppress warning
+            warning('off', 'imageio:tiffutils:libtiffWarning');
+            
+            t = Tiff(char(fullfile(ffpath, ffname)), 'r');
+            
+            %loop over frames
+            for ii = frame_lo:frame_hi
+                if ii > N_frames
+                    break;
+                end
+                setDirectory(t, ii);
+                tempImg = read(t);
+                %extract ROI for current frame
+                video(:, :, ii-frame_lo+1) = tempImg(y_lo:y_hi, x_lo:x_hi);
+            end
+            close(t);
+            
+            %re-enable warning
+            warning('on', 'imageio:tiffutils:libtiffWarning');
+            
+        otherwise
+            error('Unsupported file format');
+    end
     
 end
 
--- prepData.m
+++ prepData.m
@@ -51,6 +51,9 @@ function [] = prepData(app)
 %           initialises all class labels to -1 in preparation for labelling
 %           extracts all available information from meta data (e.g. frame rate)
 %
+%Note that a future version will handle cases where FITS meta data is not
+%available.
+%
 %Inputs
 %------
 %app    (handle)    main GUI handle
@@ -68,37 +71,98 @@ function [] = prepData(app)
 %convertLoColiToKinetics()
 %computeStepAngles()
 %computeStepAnglesRelToCell()
+%getFileExtension()             - local to this .m file
+%promptUserForMissingParams()   - local to this .m file
     
     %obtain fluorescence videos and ask user to confirm chronological order
-    [app.movie_data.params.ffFile, app.movie_data.params.ffPath] = uigetfile('*.fits', 'Select raw FITS video files:','MultiSelect', 'on');
-    app.movie_data.params.ffFile = confirmVideoOrder(app.movie_data.params.ffFile);
+    [file, path, filterIndex] = uigetfile({'*.fits;*.FITS;*.tif;*.TIF;*.tiff;*.TIFF', 'Video Files (*.fits, *.FITS, *.tif, *.TIF, *.tiff, *.TIFF)'}, 'Select raw video files:', 'MultiSelect', 'on');
+    if filterIndex == 0
+        disp('User did not select a file.');
+        return;
+    end
     
-    %obtain frame rate from KCT value in FITS file header
-    if iscell(app.movie_data.params.ffFile)
-        app.movie_data.params.frame_rate = 1/str2num(getFITSMeta(string(app.movie_data.params.ffFile(1)), app.movie_data.params.ffPath, 'KCT'));
+    %confirm chronological order of videos
+    if iscell(file)
+        app.movie_data.params.ffFile = confirmVideoOrder(file);
     else
-        app.movie_data.params.frame_rate = 1/str2num(getFITSMeta(string(app.movie_data.params.ffFile), app.movie_data.params.ffPath, 'KCT'));
+        app.movie_data.params.ffFile = file;
+    end
+    app.movie_data.params.ffPath = path;    %intentionally not assigning this to app handles at call to uigetfile in case user presses cancel
+    
+    %check the file extensions are consistent; if so get the extension
+    [file_ext, consistent] = getFileExtension(app.movie_data.params.ffFile);
+    
+    %return if filenames are inconsistent
+    if ~consistent
+        app.textout.Value = "File extensions of the selected fluorescence video files are inconsistent; data has not been prepared for analysis.";
+        return;
     end
     
-    %build frame offset index for all FITS files
-    app.movie_data.params.frame_offsets(1) = 0;
-    if iscell(app.movie_data.params.ffFile)
-        for ii = 2:size(app.movie_data.params.ffFile, 2)
-            app.movie_data.params.frame_offsets(ii) = str2num(getFITSMeta(string(app.movie_data.params.ffFile(ii-1)), app.movie_data.params.ffPath, 'NAXIS3'));
-        end
-        app.movie_data.params.frame_offsets = cumsum(app.movie_data.params.frame_offsets);
+    %handle the other tiff extension
+    if lower(file_ext) == ".tiff"
+        file_ext = ".tif";
+    end
+    
+    switch lower(file_ext) 
+        case ".fits"
+            %obtain frame rate from KCT value in FITS file header
+            if iscell(app.movie_data.params.ffFile)
+                app.movie_data.params.frame_rate = 1/str2num(getFITSMeta(string(app.movie_data.params.ffFile(1)), app.movie_data.params.ffPath, 'KCT'));
+            else
+                app.movie_data.params.frame_rate = 1/str2num(getFITSMeta(string(app.movie_data.params.ffFile), app.movie_data.params.ffPath, 'KCT'));
+            end
+            
+            %build frame offset index for all FITS files
+            app.movie_data.params.frame_offsets(1) = 0;
+            if iscell(app.movie_data.params.ffFile)
+                for ii = 2:size(app.movie_data.params.ffFile, 2)
+                    app.movie_data.params.frame_offsets(ii) = str2num(getFITSMeta(string(app.movie_data.params.ffFile(ii-1)), app.movie_data.params.ffPath, 'NAXIS3'));
+                end
+                app.movie_data.params.frame_offsets = cumsum(app.movie_data.params.frame_offsets);
+        
+                for ii = 1:size(app.movie_data.params.ffFile, 2)
+                    app.movie_data.params.frames_per_file(ii) = str2num(getFITSMeta(string(app.movie_data.params.ffFile(ii)), app.movie_data.params.ffPath, 'NAXIS3'));
+                end 
+            end
 
-        for ii = 1:size(app.movie_data.params.ffFile, 2)
-            app.movie_data.params.frames_per_file(ii) = str2num(getFITSMeta(string(app.movie_data.params.ffFile(ii)), app.movie_data.params.ffPath, 'NAXIS3'));
-        end
+        case ".tif"
+            % << future handling of TIF metadata using imfinfo() >>
+            
+            %build frame offset index for all TIF files
+            app.movie_data.params.frame_offsets(1) = 0;
+            app.movie_data.params.frames_per_file = zeros(size(app.movie_data.params.ffFile, 2), 1);
+            
+            %if there are multiple files
+            if iscell(app.movie_data.params.ffFile)
+                for ii = 1:size(app.movie_data.params.ffFile, 2)
+                    info = imfinfo(fullfile(app.movie_data.params.ffPath, app.movie_data.params.ffFile{ii}));
+                    app.movie_data.params.frames_per_file(ii) = size(info,1); %number of frames in the current TIF file
+                    
+                    %for the first file, the offset is already set to 0
+                    if ii > 1
+                        %update frame offsets for subsequent files
+                        app.movie_data.params.frame_offsets(ii) = app.movie_data.params.frame_offsets(ii-1) + app.movie_data.params.frames_per_file(ii-1);
+                    end
+                end
+            else
+                info = imfinfo(fullfile(app.movie_data.params.ffPath, app.movie_data.params.ffFile));
+                app.movie_data.params.frames_per_file = numel(info);
+            end
+
+        otherwise
+            app.textout.Value = "File type of fluorescence video is unknown; data has not been prepared for analysis.";
+            return;
     end
     
+    %prompt for any missing parameters
+    app.movie_data.params = promptUserForMissingParams(app.movie_data.params);
+
     %filter tracks
     filterTracks(app);
     
     %register all filtered track IDs
-    f = waitbar(0,'1','Name','Preparing data: registering filtered tracks');
-    waitbar(1/3, f, 'Registering filtered tracks');
+    file_ext = waitbar(0,'1','Name','Preparing data: registering filtered tracks');
+    waitbar(1/3, file_ext, 'Registering filtered tracks');
     for ii = 1:size(app.movie_data.cellROI_data)
         if ~isempty(app.movie_data.cellROI_data(ii).tracks)
             app.movie_data.cellROI_data(ii).filtered_track_IDs = unique(app.movie_data.cellROI_data(ii).tracks(:,4));
@@ -106,11 +170,11 @@ function [] = prepData(app)
     end
     
     %generate all inverted brightfield overlays
-    waitbar(2/3, f, 'Generating overlays');
+    waitbar(2/3, file_ext, 'Generating overlays');
     app.movie_data = genAllOverlays(app.movie_data, app.movie_data.params.ill_border, 8);
     
     %compute localisation-to-membrane distance for every localisation in dataset
-    waitbar(3/3, f, 'Computing distances to membrane.');
+    waitbar(3/3, file_ext, 'Computing distances to membrane (window will close on completion)');
     app.movie_data = computeLocMemDists(app.movie_data);
     
     %add all StormTracker data to the tracks matrix
@@ -195,6 +259,162 @@ function [] = prepData(app)
                                                'Following step size (nm)',...
                                                'class label'};
     
-    close(f);
+    close(file_ext);
 end
+
+
+function [file_ext, consistent] = getFileExtension(ffFile)
+%Extracts video file extension, and identifies inconsistency when multiple
+%are present, Oliver Pambos 28/02/2024
+%oliver.pambos@physics.ox.ac.uk
+%
+%
+%MATLAB FUNCTION: getFileExtension
+%AUTHOR: OLIVER JAMES PAMBOS, DEPARTMENT OF PHYSICS, UNIVERSITY OF OXFORD, UK
+%CONTACT: oliver.pambos@physics.ox.ac.uk
+%
+%LEGAL DISCLAIMER
+%THIS CODE IS INTENDED FOR USE ONLY BY INDIVIDUALS WHO HAVE RECEIVED
+%EXPLICIT AUTHORIZATION FROM THE AUTHOR, OLIVER JAMES PAMBOS. ANY FORM OF
+%COPYING, REDISTRIBUTION, OR UNAUTHORIZED USE OF THIS CODE, IN WHOLE OR IN
+%PART, IS PROHIBITED. BY USING THIS CODE, USERS SIGNIFY THAT THEY HAVE
+%READ, UNDERSTOOD, AND AGREED TO BE BOUND BY THE TERMS OF SERVICE PRESENTED
+%UPON SOFTWARE LAUNCH, INCLUDING THE REQUIREMENT FOR CO-AUTHORSHIP ON ANY
+%RELATED PUBLICATIONS. THIS APPLIES TO ALL LEVELS OF USE, INCLUDING PARTIAL
+%USE OR MODIFICATION OF THE CODE OR ANY OF ITS EXTERNAL FUNCTIONS.
+%
+%USERS ARE RESPONSIBLE FOR ENSURING FULL UNDERSTANDING AND COMPLIANCE WITH
+%THESE TERMS, INCLUDING OBTAINING AGREEMENT FROM THE APPROPRIATE
+%PUBLICATION DECISION-MAKERS WITHIN THEIR ORGANIZATION OR INSTITUTION.
+%
+%NOTE: UPON PUBLIC RELEASE OF THIS SOFTWARE, THESE TERMS MAY BE SUBJECT TO
+%CHANGE. HOWEVER, USERS OF THIS PRE-RELEASE VERSION ARE STILL BOUND BY THE
+%CO-AUTHORSHIP AGREEMENT FOR ANY USE MADE PRIOR TO THE PUBLIC RELEASE. THE
+%RELEASED VERSION WILL BE AVAILABLE FROM A DESIGNATED ONLINE REPOSITORY
+%WITH POTENTIALLY DIFFERENT USAGE CONDITIONS.
+%
+%
+%
+%Inputs
+%------
+%file_ext   (str)   file extension including `.` char
+%consistent (bool)  true if all file extensions are identical; else false
+%
+%Output
+%------
+%None
+%
+%Dependent functions (excluding callbacks)
+%-----------------------------------------
+%None
+    
+    file_ext    = "";
+    consistent  = true;
+    
+    %if multiple files are present
+    if iscell(ffFile)
+        [~, ~, file_ext] = fileparts(ffFile{1});
+        
+        %check remaining filenames for consistency
+        for ii = 2:numel(ffFile)
+            [~, ~, current_ext] = fileparts(ffFile{ii});
+            if ~strcmpi(file_ext, current_ext)
+                consistent = false;
+                break;
+            end
+        end
+    else
+        [~, ~, file_ext] = fileparts(ffFile);
+    end
+end
+
+
+function params = promptUserForMissingParams(params)
+%Prompts the user to manually enter all necessary parameters that could not
+%be read from metadata or tracking file, Oliver Pambos, 28/02/2024.
+%oliver.pambos@physics.ox.ac.uk
+%
+%
+%MATLAB FUNCTION: promptUserForMissingParams
+%AUTHOR: OLIVER JAMES PAMBOS, DEPARTMENT OF PHYSICS, UNIVERSITY OF OXFORD, UK
+%CONTACT: oliver.pambos@physics.ox.ac.uk
+%
+%LEGAL DISCLAIMER
+%THIS CODE IS INTENDED FOR USE ONLY BY INDIVIDUALS WHO HAVE RECEIVED
+%EXPLICIT AUTHORIZATION FROM THE AUTHOR, OLIVER JAMES PAMBOS. ANY FORM OF
+%COPYING, REDISTRIBUTION, OR UNAUTHORIZED USE OF THIS CODE, IN WHOLE OR IN
+%PART, IS PROHIBITED. BY USING THIS CODE, USERS SIGNIFY THAT THEY HAVE
+%READ, UNDERSTOOD, AND AGREED TO BE BOUND BY THE TERMS OF SERVICE PRESENTED
+%UPON SOFTWARE LAUNCH, INCLUDING THE REQUIREMENT FOR CO-AUTHORSHIP ON ANY
+%RELATED PUBLICATIONS. THIS APPLIES TO ALL LEVELS OF USE, INCLUDING PARTIAL
+%USE OR MODIFICATION OF THE CODE OR ANY OF ITS EXTERNAL FUNCTIONS.
+%
+%USERS ARE RESPONSIBLE FOR ENSURING FULL UNDERSTANDING AND COMPLIANCE WITH
+%THESE TERMS, INCLUDING OBTAINING AGREEMENT FROM THE APPROPRIATE
+%PUBLICATION DECISION-MAKERS WITHIN THEIR ORGANIZATION OR INSTITUTION.
+%
+%NOTE: UPON PUBLIC RELEASE OF THIS SOFTWARE, THESE TERMS MAY BE SUBJECT TO
+%CHANGE. HOWEVER, USERS OF THIS PRE-RELEASE VERSION ARE STILL BOUND BY THE
+%CO-AUTHORSHIP AGREEMENT FOR ANY USE MADE PRIOR TO THE PUBLIC RELEASE. THE
+%RELEASED VERSION WILL BE AVAILABLE FROM A DESIGNATED ONLINE REPOSITORY
+%WITH POTENTIALLY DIFFERENT USAGE CONDITIONS.
+%
+%
+%This implementation is tailored for numeric data input. To accommodate
+%string inputs in future, input parsing will be necessary to identify
+%non-numeric characters in the input. This approach allows for flexible
+%handling of various input types without relying on hardcoded values or a
+%lookup table for allowed parameters.
+%
+%Inputs
+%------
+%params (struct)    app.movie_data.params substruct that holds existing
+%                       parameters read from meta data and analysis files
+%
+%Output
+%------
+%params (struct)    app.movie_data.params substruct, now including
+%                       manually-entered parameters
+%
+%Dependent functions (excluding callbacks)
+%-----------------------------------------
+%None
+    
+    %list of required fields and corresponding user-friendly descriptions
+    required_fields     = {'frame_rate', 'px_scale'};
+    field_descriptions  = {'Frame rate (Hz)', 'Pixel scale (nm / pixel)'};
+    default_vals        = {'5', '0.096'};
+    
+    missing_fields = {};
+    missing_descriptions = {};
+    missing_defaults = {};
+    
+    %check each parameter to determine what's missing
+    for ii = 1:length(required_fields)
+        if ~isfield(params, required_fields{ii})
+            missing_fields{end+1}       = required_fields{ii};
+            missing_descriptions{end+1} = sprintf("Enter value for %s:", field_descriptions{ii});
+            missing_defaults{end+1}     = default_vals{ii};
+        end
+    end
+    
+    %prompt for all/any missing parameters
+    if ~isempty(missing_fields)
+        dlgtitle = "Input Required Parameters";
+        dims = [1 35];
+        answers = inputdlg(missing_descriptions, dlgtitle, dims, missing_defaults);
+        
+        %convert the inputs to floats and save to struct
+        for ii = 1:length(missing_fields)
+            if ~isempty(answers) && ~isempty(answers{ii})
+                params.(missing_fields{ii}) = str2double(answers{ii});
+            else
+                %handle user closing window
+                error('Input for all fields is required. Operation cancelled.');
+            end
+        end
+    end
+
+end
+
 
